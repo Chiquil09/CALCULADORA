7edb402d9b413c8bcba4df8598c7e1fe698cc1c0
Chiquil09 <114046386+Chiquil09@users.noreply.github.com>
2022-09-23 13:59:20 -0500

    Calculadora
    Calculadora2.0

---
+++ CALCULADORA-Hotfix/CALCULADORA-Hotfix/Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,17 @@
+package calculadora;
+
+/**
+ *
+ * @author kevin
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login window=new Login();
+        window.setTitle("Login");
+        window.setVisible(true);
+        
+    }
+    
+}
