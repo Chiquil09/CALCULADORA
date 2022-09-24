cad5985b6428334167ef1aae53c8b8ba41b50084
AyalaManu <114090237+AyalaManu@users.noreply.github.com>
2022-09-23 20:02:18 -0500

    Modos
    Se agrego la interfaz de la calculadora con sus respectivos botones, tanto al modo básico como al modo avanzado

--- CALCULADORA-Hotfix/CALCULADORA-Hotfix/Calculadora/src/calculadora/Login.java
+++ CALCULADORA-Hotfix/CALCULADORA-Hotfix/Calculadora/src/calculadora/Login.java
@@ -192,6 +192,9 @@ public class Login extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+         this.dispose();
+        calculadoraBasica ver = new calculadoraBasica();
+        ver.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
